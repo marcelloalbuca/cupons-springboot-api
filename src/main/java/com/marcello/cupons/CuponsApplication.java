1046b0fda6243983691d09dcbe0869e8616d739a
Marcello Albuquerque <marcello.albuca@gmail.com>
2026-02-18 23:48:23 -0300

    first commit

---
+++ src/main/java/com/marcello/cupons/CuponsApplication.java
@@ -0,0 +1,13 @@
+package com.marcello.cupons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuponsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuponsApplication.class, args);
+	}
+
+}
